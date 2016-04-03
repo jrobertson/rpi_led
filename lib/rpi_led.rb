5440ca3b775080cb696c93875532ee0e4a531dd4
Your Name <you@example.com>
2016-04-03 13:16:41 +0100

    1st commit

---
+++ lib/rpi_led.rb
@@ -0,0 +1,61 @@
+#!/usr/bin/env ruby
+
+# file: rpi_led.rb
+
+require 'rpi_pwm'
+
+
+class RPiLed < RPiPwm
+
+  attr_accessor :brightness
+
+  def initialize(pin_num, brightness: 100, smooth: true)
+
+    @smooth = smooth
+    @brightness = @duty_cycle
+    super(pin_num, duty_cycle: brightness)
+
+  end
+
+  def brighter()
+
+    return if @brightness == 100
+    increase = @brightness <= 90 ? 10 : 100 - @brightness
+    self.brightness = @brightness + increase
+    
+  end
+
+  def brightest()
+    self.brightness = 100
+  end
+
+  def brightness=(val)
+
+    return self.duty_cycle = val unless @smooth
+
+    a = if val > @duty_cycle then
+      (@duty_cycle..val).step(0.01).to_a
+    else
+      (val..@duty_cycle).step(0.01).to_a.reverse
+    end
+
+    duration = 1.5 / a.length
+    a.each {|x| self.duty_cycle = x; sleep duration }
+    @brightness = val.round(2)
+
+  end
+
+  alias bright brightness
+
+  def dimmer()
+
+    return if @brightness <= 2
+    decrease = brightness > 10 ? 10 : 2
+    self.brightness = @brightness - decrease
+
+  end
+
+  def dimmest()
+    self.brightness = 0.1
+  end
+end
---
+++ rpi_led.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name = 'rpi_led'
+  s.version = '0.1.0'
+  s.summary = 'Control the brightness of an LED (connected via GPIO) on a Raspberry Pi.'
+  s.authors = ['James Robertson']
+  s.files = Dir['lib/rpi_led.rb']
+  s.add_runtime_dependency('rpi_pwm', '~> 0.2', '>=0.2.0') 
+  s.signing_key = '../privatekeys/rpi_led.pem'
+  s.cert_chain  = ['gem-public_cert.pem']
+  s.license = 'MIT'
+  s.email = 'james@r0bertson.co.uk'
+  s.homepage = 'https://github.com/jrobertson/rpi_led'
+end
